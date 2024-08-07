47ab63054a4650886d350c9eb4cab232dcdefb45
Kim-William <130305118+Kim-William@users.noreply.github.com>
2024-08-07 15:01:04 +0200

    Delete debugging function
    Delete debugging function

--- Projects/blog-2nd/main.py
+++ Projects/blog-2nd/main.py
@@ -273,30 +273,7 @@ def contact():
         return render_template('contact.html', msg_sent=True)
     return render_template("contact.html", msg_sent=False)
 
-def smtp_setting(email, password, type=None):
-  mail_type = None
-  port = 587
-
-  if type == 'naver':
-    mail_type = 'smtp.naver.com'
-  elif type == 'gmail':
-    mail_type = 'smtp.gmail.com'
-  else:
-    mail_type = 'smtp.gmail.com'
-    
-  # SMTP 세션 생성
-  smtp = smtplib.SMTP(mail_type, port)
-  smtp.set_debuglevel(True)
-
-  # SMTP 계정 인증 설정
-  smtp.ehlo()
-  smtp.starttls() # TLS 사용시 호출
-  smtp.login(email, password) # 로그인
-
-  return smtp
-
 def send_email(name, email, phone, message):
-    
     try:
         email_message = f"Subject:{name} Sent this Blog Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
         with smtplib.SMTP("smtp.gmail.com") as connection:
